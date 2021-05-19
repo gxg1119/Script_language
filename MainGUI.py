1bbc4e6009164968c64af91e41429c32a6a724c9
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2021-05-19 19:40:50 +0900

    MainGUI 초기 설정
    타이틀 삽입, 창 크기 지정, 배경 색 임시 지정

---
+++ MainGUI.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import font
+
+class MainGUI:
+    def __init__(self):
+        window = Tk()
+        window.title('Accident')
+        window['bg'] = 'old lace'
+        window.geometry("600x850")
+
+
+        window.mainloop()
+
+MainGUI()
