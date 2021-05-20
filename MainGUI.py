ebf9cce51e9bf533e1c288a855e303a3a67de35b
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2021-05-20 17:09:10 +0900

    여러가지 수정 중

--- MainGUI.py
+++ MainGUI.py
@@ -3,7 +3,8 @@ from tkinter import font
 
 window = Tk()
 window.title('Accident')
-window['bg'] = 'old lace'
+color = 'SkyBlue1'
+window['bg'] = color
 window.geometry("600x850")
 
 # 프로그램 이름 UI
@@ -11,13 +12,13 @@ def InitLogo():
     TempFont_1 = font.Font(window, size=70, weight='bold', family='1훈떡볶이 Regular')
     MainText = Label(window, font=TempFont_1, text="Accident")
     MainText.pack()
-    MainText['bg'] = 'old lace'
+    MainText['bg'] = color
     MainText.place(x=10, y=10)
 
     TempFont_2 = font.Font(window, size=30, weight='bold', family='1훈떡볶이 Regular')
     SubText = Label(window, font=TempFont_2, text="민방위 대피시설 위치")
     SubText.pack()
-    SubText['bg'] = 'old lace'
+    SubText['bg'] = color
     SubText.place(x=15, y=90)
 
 # 검색 창 UI
@@ -39,64 +40,69 @@ def InitSearchButton():
 def SearchButtonAction(self):
     pass
 
-# 대피시설 도로명 주소 Text UI
-def InitRoadAddressBox():
+# 각 경기도 시 마다의 로고 이미지 UI
+def InitImage():
+    image = PhotoImage(file="image/1.png").subsample(2,2)
+    imageLabel= Label(window,image=image, width = 180, height = 180, relief= 'raised')
+    imageLabel.image = image
+    imageLabel.pack()
+    imageLabel.place(x=380, y = 15)
+
+# 대피시설 도로명 주소 ListBox UI
+def RoadAddressBox():
     global RoadAddressBox
 
     frame = Frame(window)
 
     RATScrollbar = Scrollbar(frame)
     RATScrollbar.pack(side = 'right', fill = 'y')
-    #RATScrollbar.place(x=550, y=300)
 
     TempFont = font.Font(frame, size = 15, family='1훈떡볶이 Regular')
     RoadAddressBox = Listbox(frame, font = TempFont, width = 53, height = 7, borderwidth = 10,
                                relief='ridge',yscrollcommand=RATScrollbar.set)
 
     RoadAddressBox.pack(side = 'left')
-    #RoadAddressBox.place(x = 15, y = 220)
     RATScrollbar["command"]=RoadAddressBox.yview
-
-    RoadAddressBox.insert(1, "도서관")
-    RoadAddressBox.insert(2, "도서관")
-    RoadAddressBox.insert(3, "도서관")
-    RoadAddressBox.insert(4, "도서관")
-    RoadAddressBox.insert(5, "도서관")
-    RoadAddressBox.insert(6, "도서관")
-    RoadAddressBox.insert(7, "도서관")
-    RoadAddressBox.insert(8, "씨발")
-    RoadAddressBox.insert(9, "도서관")
-    RoadAddressBox.insert(10, "도서관")
-    RoadAddressBox.insert(11, "도서관")
-    RoadAddressBox.insert(12, "도서관")
 
     frame.pack()
     frame.place(x = 14, y = 220)
-    #RATScrollbar.config(command=RoadAddressBox.yview)
+
+# 대피시설 정보 Text UI
+def Info_Shelter():
+    TempFont = font.Font(window, size=15, family='1훈떡볶이 Regular')
+    text = Text(window, font= TempFont, width=30, height=20, borderwidth = 10,
+                relief = 'ridge')
+    text.pack()
+    text.place(x=15, y=400)
 
 # 메일 서비스 버튼 UI
 def MailButton():
-    photo = PhotoImage(file="image/Gmail.png").subsample(11,11)
+    photo = PhotoImage(file="image/Gmail1.png").subsample(4,5)
     SearchButton = Button(window, image=photo)
     SearchButton.image = photo
+    SearchButton['bg'] = 'old lace'
     SearchButton.pack()
     SearchButton.place(x=330, y=680)
 
 # 지도 서비스 버튼 UI
 def MapButton():
-    photo = PhotoImage(file="image/지도.png").subsample(11,11)
+    photo = PhotoImage(file="image/gmap.png").subsample(5,5)
     SearchButton = Button(window, image=photo)
     SearchButton.image = photo
+    SearchButton['bg'] = 'old lace'
     SearchButton.pack()
-    SearchButton.place(x=460, y=680)
+    SearchButton.place(x=470, y=680)
 
 
 InitLogo()
 InitInPutLabel()
 InitSearchButton()
-#InitImage()
+InitImage()
+
+RoadAddressBox()
+Info_Shelter()
+
 MailButton()
 MapButton()
-InitRoadAddressBox()
 
 window.mainloop()
