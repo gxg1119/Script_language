e9286eae61ec0d6ab5a5c7cdfd4ec5c89f476c26
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2021-05-20 13:49:18 +0900

    검색 창, 검색 버튼 추가

--- MainGUI.py
+++ MainGUI.py
@@ -7,19 +7,40 @@ class MainGUI:
         self.window.title('Accident')
         self.window['bg'] = 'old lace'
         self.window.geometry("600x850")
-        
+
         self.InitLogo()
+        self.InitInPutLabel()
+        self.InitSearchButton()
         self.window.mainloop()
 
     def InitLogo(self):
         TempFont_1 = font.Font(self.window, size=70, weight='bold', family='1훈떡볶이 Regular')
         MainText = Label(self.window, font=TempFont_1, text="Accident")
         MainText.pack()
+        MainText['bg'] = 'old lace'
         MainText.place(x=10, y=10)
 
         TempFont_2 = font.Font(self.window, size=30, weight='bold', family='1훈떡볶이 Regular')
         SubText = Label(self.window, font=TempFont_2, text="민방위 대피시설 위치")
         SubText.pack()
+        SubText['bg'] = 'old lace'
         SubText.place(x=15, y=90)
+
+    def InitInPutLabel(self):
+        global InPutLabel
+        TempFont = font.Font(self.window, size=20, weight='bold', family='1훈떡볶이 Regular')
+        InPutLabel = Entry(self.window, font=TempFont, width=15, borderwidth=10, relief='ridge')
+
+        InPutLabel.pack()
+        InPutLabel.place(x=15, y=155)
+
+    def InitSearchButton(self):
+        TempFont = font.Font(self.window, size=20, weight='bold', family='1훈떡볶이 Regular')
+        SearchButton = Button(self.window, font=TempFont, text="검색", command=self.SearchButtonAction)
+        SearchButton.pack()
+        SearchButton.place(x=270, y=155)
+
+    def SearchButtonAction(self):
+        pass
 
 MainGUI()
