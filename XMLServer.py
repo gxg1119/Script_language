e79b16c682671b80a45b697967e305d0152e6df4
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2021-05-26 17:33:32 +0900

    데이터 정보 받아오기 수정
    없는 정보들은 정보가 없다는 text로 변경

--- XMLServer.py
+++ XMLServer.py
@@ -39,20 +39,54 @@ def XmlToList(temp):
     tree = ElementTree.fromstring(temp)
 
     for shelter in tree.findall('./row'):
+
         City = shelter.find('SIGUN_NM')                     # 시군명
+        if City.text == None:
+            City.text = '시군명 정보 없음'
+
         Shelter_Name = shelter.find('BIZPLC_NM')            # 대피시설명
+        if Shelter_Name.text == None:
+            Shelter_Name.text = '대피시설명 정보 없음'
+
         License_date = shelter.find('LICENSG_DE')           # 인허가 일자
+        if License_date.text == None:
+            License_date.text = '인허가 일자 정보 없음'
+
         Open = shelter.find('BSN_STATE_NM')                 # 운영상태
+        if Open.text == None:
+            Open.text = '운영상태 정보 없음'
+
         Area = shelter.find('LOCPLC_AR_INFO')               # 소재지 면적정보
-        RoadAddress = shelter.find('REFINE_ROADNM_ADDR')    # 소재지 도로명 주소
+        if Area.text == None:
+            Area.text = '면적 정보 없음'
+
+        RoadAddress = shelter.find('REFINE_ROADNM_ADDR')     # 소재지 도로명 주소
+        if RoadAddress.text == None:
+            RoadAddress.text = '도로명 주소 정보 없음'
+
         Address = shelter.find('REFINE_LOTNO_ADDR')         # 소재지 지번 주소
+        if Address.text == None:
+            Address.text = '지번 주소 정보 없음'
+
         Post = shelter.find('ROADNM_ZIP_CD')                # 소재지 우편 번호
+        if Post.text == None:
+            Post.text = '우편번호 정보 없음'
+
         Public_Private = shelter.find('FACLT_DIV_NM')       # 시설 구분명 (공공, 민간)
+        if Public_Private.text == None:
+            Public_Private.text = '시설 구분 정보 없음'
+
         Lat = shelter.find('REFINE_WGS84_LAT')              # 위도
+        if Lat.text == None:
+            Lat.text = '위도 정보 없음'
+
         Long = shelter.find('REFINE_WGS84_LOGT')            # 경도
+        if Long.text == None:
+            Long.text = '경도 정보 없음'
 
         DataList.append([City.text, Shelter_Name.text, License_date.text, Open.text, Area.text,
                          RoadAddress.text, Address.text, Post.text, Public_Private.text, Lat.text, Long.text])
 
-for i in DataList:
-    print(i)
+
+#for i in DataList:
+    #print(i)
